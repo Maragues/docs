347dd5dc3307ad6f997f56927dc6a6d853a81723
Ajay Kelkar <ajay.kelkar@ory.sh>
2022-12-09 15:58:14 +0100

    docs: adding documentation for advanced settings

--- src/sidebar.js
+++ src/sidebar.js
@@ -310,6 +310,11 @@ module.exports = {
     },
     {
       type: "category",
+      label: "Advanced",
+      items: ["advanced/settings"],
+    },
+    {
+      type: "category",
       label: "Actions",
       items: ["guides/integrate-with-ory-cloud-through-webhooks"],
     },
